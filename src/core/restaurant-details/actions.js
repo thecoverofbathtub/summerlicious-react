d057bb8d194887f33edf7ff7a67a6c328206a3da
thecoverofbathtub <gao.yang.ustc@gmail.com>
2016-07-25 16:24:28 -0400

    Use import instead of require to load JSON

--- src/core/restaurant-details/actions.js
+++ src/core/restaurant-details/actions.js
@@ -2,11 +2,13 @@ import {
     LOAD_DETAILS_SUCCESS
 } from './action-types';
 
+import { details } from 'json!./details.json';
+
 export function loadRestaurantDetails() {
     return (dispatch) => {
         dispatch({
             type: LOAD_DETAILS_SUCCESS,
-            payload: require('json!./details.json')
+            payload: details
         });
     };
 }
